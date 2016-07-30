de4673baf4ece1af9aadd24d014e6b5eefe59018
Aditia A. Pratama <aditia.ap@gmail.com>
2016-07-30 07:33:14 +0700

    fork and refactor from OHA Layout Tools

---
+++ PSLayout.py
@@ -0,0 +1,13 @@
+import bpy
+
+bl_info = {
+    "name": "PSLayout",
+    "author": "Adhi Hargo, Johan Tri Handoyo, Aditia A. Pratama",
+    "version": (1, 0, 4),
+    "blender": (2, 77, 0),
+    "location": "Sequencer > Tools > PSLayout",
+    "description": "Create layout files.",
+    "warning": "",
+    "wiki_url": "https://github.com/pawitrastudio/PSLayout",
+    "tracker_url": "https://github.com/pawitrastudio/PSLayout/issues",
+    "category": "Sequencer"}
